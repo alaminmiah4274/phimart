97946fff83b8f09b1ff07468152f3a6dca375f6c
AlAminMiah4274 <alaminmiah4274@gmail.com>
2025-05-01 19:44:04 +0600

    changed permissions in review class

--- product/permissions.py
+++ product/permissions.py
@@ -5,13 +5,13 @@ class IsReviewAuthorOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
-        return bool(request.user and request.user.is_staff)
+        return bool(request.user and request.user.is_authenticated)
 
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
 
-        if request.user.is_superuser:
+        if request.user.is_staff:
             return True
 
         return obj.user == request.user
